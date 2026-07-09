0bced981b825e25b2d0415481334553253292165
Jacob Plourde <jacob_plourde@hotmail.com>
2026-07-09 13:37:05 -0600

    Move DMA init to the header

--- hal/stm32f1/private/dma.h
+++ hal/stm32f1/private/dma.h
@@ -1,6 +1,10 @@
 #ifndef _EPSDKX_HAL_DMA_H
 #define _EPSDKX_HAL_DMA_H
 
-void hal_dma_init(void);
+#include "stm32f1xx.h"
+
+static inline void hal_dma_init(void) {
+  RCC->AHBENR |= RCC_AHBENR_DMA1EN;
+}
 
 #endif
