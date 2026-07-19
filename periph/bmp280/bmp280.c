023713f8132b20e24f1fbe211fa5c38200e4b0ce
Jacob Plourde <jacob_plourde@hotmail.com>
2026-07-19 14:24:28 -0600

    BMP280 configuration and measurement compensation functions

--- periph/bmp280/bmp280.c
+++ periph/bmp280/bmp280.c
@@ -10,7 +10,32 @@
 #include <stdint.h>
 
 
+typedef struct bmp280_compensation_params_s {
+  uint16_t dig_T1;
+  int16_t dig_T2;
+  int16_t dig_T3;
+  uint16_t dig_P1;
+  int16_t dig_P2;
+  int16_t dig_P3;
+  int16_t dig_P4;
+  int16_t dig_P5;
+  int16_t dig_P6;
+  int16_t dig_P7;
+  int16_t dig_P8;
+  int16_t dig_P9;
+} bmp280_compensation_params_s;
+
+static bmp280_compensation_params_s params;
+
+static int32_t t_fine;
+
+
+static inline void get_compensation_params(bmp280_dev_s *dev);
+static inline void configure_measurements(bmp280_dev_s *dev);
+
 int bmp280_init_spi(bmp280_dev_s *dev, spi_channel_t channel, gpio_pin_u csb) {
+  int ret;
+
   dev->type = BMP280_SPI;
   dev->channel.spi = channel;
   dev->spi_csb = csb;
@@ -19,15 +44,32 @@ int bmp280_init_spi(bmp280_dev_s *dev, spi_channel_t channel, gpio_pin_u csb) {
   gpio_configure(&csb, GPIO_OUT);
   gpio_write(&csb, GPIO_HIGH);
 
-  return spi_init(channel, SPI_MASTER, SPI_CPOL0, SPI_CPHA0); 
+
+  ret = spi_init(channel, SPI_MASTER, SPI_CPOL0, SPI_CPHA0); 
+
+  if (ret >= 0) {
+    get_compensation_params(dev);
+    configure_measurements(dev);
+  }
+
+  return ret;
 }
 
 int bmp280_init_i2c(bmp280_dev_s *dev, i2c_channel_t channel, gpio_state_e sdo_state) {
+  int ret;
+
   dev->type = BMP280_I2C;
   dev->channel.i2c = channel;
   dev->i2c_addr = 0xE6 | sdo_state;
 
-  return i2c_init_master(channel);
+  ret = i2c_init_master(channel);
+
+  if (ret >= 0) {
+    get_compensation_params(dev);
+    configure_measurements(dev);
+  }
+
+  return ret;
 }
 
 void bmp280_write(bmp280_dev_s *dev, bmp280_addr_e addr, uint8_t tx) {
@@ -84,4 +126,61 @@ void bmp280_readn(bmp280_dev_s *dev, bmp280_addr_e addr, uint8_t *rx, uint8_t n)
       }
       break;
   }
+}
+
+// Returns temperature in DegC, double precision. Output value of “51.23” equals 51.23 DegC.
+// t_fine carries fine temperature as global value
+double bmp280_compensate_temperature(int32_t adc_t) {
+  double var1, var2, t;
+
+  var1 = (((double)adc_t) / 16384.0 - ((double)params.dig_T1) / 1024.0) *
+         ((double)params.dig_T2);
+
+  var2 = ((((double)adc_t) / 131072.0 - ((double)params.dig_T1) / 8192.0) *
+         (((double)adc_t) / 131072.0 - ((double)params.dig_T1) / 8192.0)) *
+         ((double)params.dig_T3);
+
+  t_fine = (int32_t)(var1 + var2);
+  t = (var1 + var2) / 5120.0;
+  return t;
+}
+
+// Returns pressure in Pa as double. Output value of “96386.2” equals 96386.2 Pa = 963.862 hPa
+double bmp280_compensate_pressure(int32_t adc_p) {
+  double var1, var2, p;
+  var1 = ((double)t_fine / 2.0) - 64000.0;
+
+  var2 = var1 * var1 * ((double)params.dig_P6) / 32768.0;
+  var2 = var2 + var1 * ((double)params.dig_P5) * 2.0;
+  var2 = (var2 / 4.0) + (((double)params.dig_P4) * 65536.0);
+
+  var1 = (((double)params.dig_P3) * var1 * var1 / 524288.0 + ((double)params.dig_P2) * var1) /
+         524288.0;
+  var1 = (1.0 + var1 / 32768.0) * ((double)params.dig_P1);
+
+  // Avoid exception caused by division by zero
+  if (var1 == 0.0) {
+    return 0;
+  }
+
+  p = 1048576.0 - (double)adc_p;
+  p = (p - (var2 / 4096.0)) * 6250.0 / var1;
+
+  var1 = ((double)params.dig_P9) * p * p / 2147483648.0;
+  var2 = p * ((double)params.dig_P8) / 32768.0;
+
+  p = p + (var1 + var2 + ((double)params.dig_P7)) / 16.0;
+  return p;
+}
+
+static inline void get_compensation_params(bmp280_dev_s *dev) {
+  bmp280_readn(dev, 0x88, (uint8_t *)&params, sizeof(bmp280_compensation_params_s));
+}
+
+static inline void configure_measurements(bmp280_dev_s *dev) {
+  // Set 62.5 ms standby, IIR filter coefficient of 16, and 4-wire spi mode
+  bmp280_write(dev, BMP280_CONFIG_ADDR, 0x3C);
+
+  // Set 1x temperature oversampling, 4x pressure oversampling, and normal mode
+  bmp280_write(dev, BMP280_CTRL_MEAS_ADDR, 0x2F);
 }
