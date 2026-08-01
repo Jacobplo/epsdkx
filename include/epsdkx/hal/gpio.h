f40c11c532a531f8d626127815529091d2b76074
Jacob Plourde <jacob_plourde@hotmail.com>
2026-07-31 19:47:44 -0600

    GPIO / I2C HAL documentation

--- include/epsdkx/hal/gpio.h
+++ include/epsdkx/hal/gpio.h
@@ -1,14 +1,48 @@
+/**
+* Internal contract for HAL GPIO implementation
+*
+* Unless otherwise stated, all functions that return int should return a
+* negative errno on error, and 0 on success.
+*/
+
 #ifndef _EPSDKX_HAL_GPIO_H
 #define _EPSDKX_HAL_GPIO_H
 
 #include <epsdkx/common/gpio.h>
 
+/**
+ * Should initialize all available GPIO interfaces / ports. No pin
+ * configuration should be handled here.
+ *
+ * It is expected that this function be called before any other gpio function
+ * at the user-level.
+ */
 void hal_gpio_init(void);
 
+/**
+ * Should configure a GPIO pin in a given input or output mode, such that it
+ * can be set (in output mode) or read from (in input mode) immediately after.
+ *
+ * Should perform validation on mode in the case that the hardware does not
+ * support all modes in gpio_mode_e, and return appropraite (negative) errnos
+ * where necessary.
+ */
 int hal_gpio_configure(gpio_pin_u *pin, gpio_mode_e mode);
 
+/**
+ * Should set a GPIO pin in and output mode to the provided logic state.
+ *
+ * Should not perform any validation to confirm the pin is in the correct
+ * mode; this is a user-level responsibility.
+ */
 void hal_gpio_write(gpio_pin_u *pin, gpio_state_e state);
 
+/**
+ * Should read and return logic state of a GPIO pin in an input mode.
+ *
+ * Should not perform any validation to confirm the pin is in the correct
+ * mode; this is a user-level responsibility.
+ */
 gpio_state_e hal_gpio_read(gpio_pin_u *pin);
 
 #endif
--- include/epsdkx/hal/i2c.h
+++ include/epsdkx/hal/i2c.h
@@ -1,3 +1,19 @@
+/**
+* Internal contract for HAL I2C interface implementation
+*
+* Is it the responsibilty of the implementation to provide a way to map
+* an input i2c_channel_t to the interface's hardware. All methods that
+* accept a 'channel' argument should perform validation to confirm that
+* the interface is valid.
+*
+* All functions should be able to perform their respective operations
+* on interfaces in either slave or master mode, so the mode should be
+* part of the internal mapping.
+*
+* Unless otherwise stated, all functions that return int should return a
+* negative errno on error, and 0 on success.
+*/
+
 #ifndef _EPSDKX_HAL_I2C_H
 #define _EPSDKX_HAL_I2C_H
 
@@ -7,16 +23,47 @@
 #include <stdint.h>
 
 
+/**
+ * Should initialize the given I2C channel only, with the provided mode,
+ * and if in slave mode, with the provided address.
+ */
 int hal_i2c_init(i2c_channel_t channel, i2c_mode_e mode, uint16_t address);
 
-const i2c_pins_s *hal_i2c_get_pins(i2c_channel_t channel);
-
+/**
+ * Should send n contiguous data packets (bytes) in a single transmission.
+ * This means that n bytes must be sent between a single START and STOP signal.
+ *
+ * This function may or may not be implemented using interrupts.
+ */
 int hal_i2c_writen(i2c_channel_t channel, uint8_t *tx, uint16_t n, uint16_t slave_address);
 
+/**
+ * Should receive n contiguous data packets (bytes) in a single transmission.
+ * This means that n bytes must be received between a single START and STOP 
+ * signal.
+ *
+ * This function may or may not be implemented using interrupts, but must always
+ * read the data into the internal RX buffer to ensure consistency for the 
+ * public API.
+ */
 int hal_i2c_readn(i2c_channel_t channel, uint16_t n, uint16_t slave_address);
 
+/**
+ * Should return the value returned by hal_rx_buffer_get() on the internal
+ * RX buffer.
+ */
 int hal_i2c_get(i2c_channel_t channel, uint8_t *rx);
 
+/**
+ * Should returns the GPIO pins associated with the interface, which should be
+ * available in the internal interface mapping.
+ */
+const i2c_pins_s *hal_i2c_get_pins(i2c_channel_t channel);
+
+/**
+ * Should return true if the bus the interface is on is still being used for
+ * I2C transmision. Otherwise, return false.
+ */
 bool hal_i2c_is_busy(i2c_channel_t channel);
 
 #endif
