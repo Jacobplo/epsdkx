86de3f0bcd4a7980941db984bc6605b27d8c4dad
Jacob Plourde <jacob_plourde@hotmail.com>
2026-08-09 11:30:50 -0600

    Finish at6558_csip_construct_frame()

--- periph/at6558/at6558.c
+++ periph/at6558/at6558.c
@@ -14,7 +14,7 @@
 #endif
 
 #if CONFIG_RX_BUFFER_SIZE < AT6558_FRAME_SIZE
-#error CONFIG_RX_BUFFER_SIZE must be at least 88 to use the AT6558 driver
+#error CONFIG_RX_BUFFER_SIZE must be at least 128 to use the AT6558 driver
 #endif
 
 
@@ -24,13 +24,27 @@ typedef enum at6558_csip_message_e {
   AT6558_MESSAGE_COUNT,
 } at6558_csip_message_e;
 
-#define CSIP_HEADER   0xBA, 0xCE
-#define BYTE(n,p)     ((uint8_t)(((n) >> (8 * (p))) & 0xFF))
-#define CSIP_LEN(n)   BYTE(n, 0), BYTE(n, 1)
-#define CSIP_CKSUM(n) BYTE(n, 0), BYTE(n, 1), BYTE(n, 2), BYTE(n, 3)
+#define CSIP_HEADER      0xBA, 0xCE
 
-#define NAV_PV        0x01, 0x03
-#define CFG_MSG       0x06, 0x01
+#define BYTE_SPLIT(n,p)  ((uint8_t)(((n) >> (8 * (p))) & 0xFF))
+#define BYTE_JOIN(n,p)   (*((n) + (p)) << (8 * (p)))
+
+#define SPLIT2(n)        BYTE_SPLIT(n, 0), BYTE_SPLIT(n, 1)
+#define SPLIT4(n)        BYTE_SPLIT(n, 0), BYTE_SPLIT(n, 1), BYTE_SPLIT(n, 2), BYTE_SPLIT(n, 3)
+#define JOIN2(n)         (BYTE_JOIN(n, 0) | BYTE_JOIN(n, 1))
+#define JOIN4(n)         (BYTE_JOIN(n, 0) | BYTE_JOIN(n, 1) | BYTE_JOIN(n, 2) | BYTE_JOIN(n, 3))
+
+#define CSIP_LEN(n)      SPLIT2(n)
+#define CSIP_CKSUM(n)    SPLIT4(n)
+
+#define CSIP_HEADER_POS  0
+#define CSIP_LEN_POS     2
+#define CSIP_CLASS_POS   4
+#define CSIP_ID_POS      5
+#define CSIP_PAYLOAD_POS 6
+
+#define NAV_PV           0x01, 0x03
+#define CFG_MSG          0x06, 0x01
 
 /**
  * Contains pre-defined CSIP messages used by the driver, without a checksum.
@@ -59,6 +73,8 @@ static void at6558_write_frame(at6558_dev_s *dev, size_t n);
 /**
  * Copies frame bytes from the provided message frame to dev->frame,
  * and also computes the frame checksum.
+ *
+ * Writes the frame after copying the full frame to dev->frame.
  */
 static void at6558_csip_construct_frame(at6558_dev_s *dev, at6558_csip_message_e message);
 
@@ -86,6 +102,8 @@ int at6558_init(at6558_dev_s *dev, uart_channel_t channel) {
     }
 
     uart_flush(dev->channel);
+
+    at6558_csip_construct_frame(dev, AT6558_POLL_NAV_PV);
   }
 
   return ret;
@@ -104,6 +122,21 @@ static void at6558_write_frame(at6558_dev_s *dev, size_t n) {
 static void at6558_csip_construct_frame(at6558_dev_s *dev, at6558_csip_message_e message) {
   const uint8_t *const frame = csip_messages[message];
 
-  (void)dev;
-  (void)frame;
+  const size_t payload_len = JOIN2(&frame[CSIP_LEN_POS]);
+
+  // Message length minus the checksum. Header + length + type = 6.
+  const size_t len = payload_len + 6;
+
+  // Compute checksum
+  uint32_t ckSum = (frame[CSIP_ID_POS] << 24) + (frame[CSIP_CLASS_POS] << 16) + payload_len;
+  for (size_t i = 0; i < payload_len; i += 4) {
+    ckSum += JOIN4(&frame[CSIP_PAYLOAD_POS + i]);
+  }
+
+  uint8_t ckSumSplit[4] = { SPLIT4(ckSum) };
+
+  memcpy(dev->frame, frame, len);
+  memcpy(dev->frame + len, ckSumSplit, 4);
+
+  at6558_write_frame(dev, len + 4);
 }
