ab645f74bcb7779b58f25169ce83c75fde12194a
Jacob Plourde <jacob_plourde@hotmail.com>
2026-08-16 13:36:26 -0600

    SD card SPI init

--- periph/sd/sd.c
+++ periph/sd/sd.c
@@ -4,12 +4,30 @@
 #include <epsdkx/common/spi.h>
 #include <epsdkx/drivers/gpio.h>
 #include <epsdkx/common/gpio.h>
+#include <epsdkx/drivers/time.h>
 
 
 int sd_init(sd_dev_s *dev, spi_channel_t channel, gpio_pin_u cs_pin) {
-  (void)dev;
-  (void)channel;
-  (void)cs_pin;
+  int ret;
+  uint8_t discard;
 
-  return 0;
+  dev->channel = channel;
+  dev->cs = cs_pin;
+
+  ret = spi_init(dev->channel, SPI_MASTER, SPI_CPOL0, SPI_CPHA0);
+  gpio_write(&dev->cs, GPIO_HIGH);
+  time_delay_ms(5);
+
+  // Set frequency to 200 kHz, inside the 100-400 kHz range for initialization.
+  spi_set_freq(dev->channel, 200);
+
+  if (ret >= 0) {
+    // Send 80 clock pulses to satisfy the 74 clock pulse requirement for initialization.
+    for (size_t i = 0; i < 10; i++) {
+      spi_read_write(dev->channel, 0);
+    }
+    spi_discardn(dev->channel, 10);
+  } 
+
+  return ret;
 }
